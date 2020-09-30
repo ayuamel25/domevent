91828120002d83737a6efe4c7e8204934566ccb5
ayuamel25 <anjnirzka217@gmail.com>
2020-09-29 19:48:41 -0700

    update semua

--- main.js
+++ main.js
@@ -10,10 +10,21 @@ const showModal = () => {
 
     modal.style.display = 'flex'
 }
-
+//kita buat fungsi yang akan di hide element modal 
 const hideModal = () => {
-
+    //kita ambil element yang akan di hide 
     let modal = document.querySelector('.modalmenu')
 
+    //kita atur style dari element tersebut
+    //jadikan dia element tersembunyi
     modal.style.display = 'none'
+}
+
+//kita buat sebuah fungsi yang akan hide about alert
+const hideaboutalert = () => {
+    //kita ambil element yang akan di hide
+    let AboutAlert = document.querySelector('.aboutAlert')
+
+    //kita atur element dari element tersebut jadikan tersembunyi
+    AboutAlert.style.display = 'none'
 }
